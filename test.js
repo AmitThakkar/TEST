d4d8503da9f87d9306da7db8d211058da0aaad35
Amit Thakkar <vigildbest@gmail.com>
2015-10-27 19:44:11 -0700

    adding sample common test file.

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 ((module) => {
+    'use strict';
     module.exports = function (request, response) {
         console.log("Hello From Handler 1");
+        let common = require('./common');
+        common.test();
+        common.test2();
         getSchema("test", "employee", function (Employee) {
             Employee.find({}, (error, employees) => {
                 response.status(200).json({employees: employees});
