3b3b61a19e4ff9884a3731258d43ef5253c048b2
Amit Thakkar <vigildbest@gmail.com>
2015-11-20 11:24:33 +0530

    adding api and module folders.

--- api/test.js
+++ api/test.js
@@ -1,9 +1,9 @@
 ((module, apiServer) => {
     'use strict';
     module.exports = function (request, response) {
-        console.log("Hello From Handler 1");
-        apiServer.modules.common.test();
-        apiServer.modules.common.test2();
+        logger.debug("Hello From Handler 2");
+        apiServer.modules.test.test();
+        apiServer.modules.test.test2();
         apiServer.getSchema("test", "employee", function (Employee) {
             Employee.find({}, (error, employees) => {
                 response.status(200).json({employees: employees});
---
+++ module/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Amit Thakkar on 10/27/15.
+ */
+((module) => {
+    module.exports = {
+        test: function() {
+            logger.debug('this is my demo edit functionality');
+        },
+        test2: function() {
+            logger.debug('this is my demo functionality');
+        }
+    }
+})(module);
