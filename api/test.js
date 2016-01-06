1100e18063c0e05dc1b035c54c081ca92c15ec64
Amit Thakkar <vigildbest@gmail.com>
2016-01-06 16:04:22 +0530

    Using third party modules.

--- api/test.js
+++ api/test.js
@@ -4,6 +4,7 @@
         logger.debug("Hello From Handler 2");
         apiServer.modules.test.test();
         apiServer.modules.test.test2();
+        logger.info('Third Party Module Test', apiServer.modules.lodash.chunk([1, 2, 3, 4], 2));
         apiServer.getSchema("test", "employee", function (Employee) {
             Employee.find({}, (error, employees) => {
                 response.status(200).json({employees: employees});
